a9bfb17b168d196b5074126ebf95615d998fca31
sklose <sebastian.klose@walmart.com>
2020-01-15 13:36:30 -0500

    test lint failure

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -5,7 +5,7 @@ This source code is licensed under the Apache 2.0 license found in the
 LICENSE file in the root directory of this source tree.
 */
 
-export * from "./model";
+ export * from "./model";
 export * from "./snapshot";
 export * from "./utils";
 export * from "./defaults";
