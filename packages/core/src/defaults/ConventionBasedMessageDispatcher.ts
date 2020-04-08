c1c4adbac0165f3b389ff33572b599a6c84e1c00
Plamen Ivanov <plamen.ivanov@jet.com>
2020-04-07 23:10:06 -0400

    Missed files

--- packages/core/src/defaults/ConventionBasedMessageDispatcher.ts
+++ packages/core/src/defaults/ConventionBasedMessageDispatcher.ts
@@ -5,7 +5,13 @@ This source code is licensed under the Apache 2.0 license found in the
 LICENSE file in the root directory of this source tree.
 */
 
-import { IDispatchContext, IMessage, IMessageDispatcher } from "../model";
+import {
+    IDispatchContext,
+    IMessage,
+    IMessageDispatcher,
+    IValidateResult,
+    NoInvalidHandlerError,
+} from "../model";
 import { prettyEventName } from "../utils";
 
 export class ConventionBasedMessageDispatcher implements IMessageDispatcher {
@@ -18,43 +24,47 @@ export class ConventionBasedMessageDispatcher implements IMessageDispatcher {
         return func !== undefined;
     }
 
-    public async dispatch(msg: IMessage, ctx: IDispatchContext): Promise<void> {
-        const type = prettyEventName(msg.type);
-        const calls: [string, any, boolean][] = [
-            ["before", msg, false],
-            [`on${type}`, msg.payload, true],
-            ["after", msg, false],
-        ];
+    public async dispatch(
+        msg: IMessage,
+        ctx: IDispatchContext,
+        metadata: { validation: IValidateResult }
+    ): Promise<any> {
+        if (metadata.validation.success) {
+            const type = prettyEventName(msg.type);
+            const calls: [string, any, boolean][] = [
+                ["before", msg, false],
+                [`on${type}`, msg.payload, true],
+                ["after", msg, false],
+            ];
 
-        let result: any | undefined;
-        for (const [name, msg, store] of calls) {
-            const func = this.target[name];
+            let result: any | undefined;
+            for (const [name, msg, store] of calls) {
+                const func = this.target[name];
+                if (func) {
+                    let val = func.apply(this.target, [msg, ctx]);
+                    if (this.isPromise(val)) {
+                        val = await val;
+                    }
+
+                    if (store) {
+                        result = val;
+                    }
+                }
+            }
+
+            return result;
+        } else {
+            const invalid = "invalid";
+            const func = this.target[invalid];
             if (func) {
-                let val = func.apply(this.target, [msg, ctx]);
-                if (this.isPromise(val)) {
-                    val = await val;
-                }
-
-                if (store) {
-                    result = val;
-                }
+                await func.apply(this.target, [msg, ctx]);
+            } else {
+                ctx.logger.error("received invalid message", metadata.validation.message, {
+                    type: msg.payload.type,
+                });
+                throw new NoInvalidHandlerError();
             }
         }
-
-        return result;
-    }
-
-    public async invalid(msg: IMessage, ctx: IDispatchContext): Promise<boolean> {
-        const invalid = "invalid";
-        const func = this.target[invalid];
-        if (func) {
-            let val = func.apply(this.target, [msg, ctx]);
-            if (this.isPromise(val)) {
-                val = await val;
-            }
-            return true;
-        }
-        return false;
     }
 
     private isPromise(val: any): val is Promise<void> {
