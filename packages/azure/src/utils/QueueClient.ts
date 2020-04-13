a898d4c09a28242f665f67e4f572ad659bebf10f
Sean Halpin <do-not-reply@example.com>
2020-04-13 15:52:48 +0100

    createQueueService with url

--- packages/azure/src/utils/QueueClient.ts
+++ packages/azure/src/utils/QueueClient.ts
@@ -113,7 +113,7 @@ export class QueueClient implements IRequireInitialization {
         this.logger = DefaultComponentContext.logger;
 
         const { retryCount, retryInterval } = config;
-        this.queueService = createQueueService(config.storageAccount, config.storageAccessKey);
+        this.queueService = createQueueService(config.storageAccount, config.storageAccessKey, config.url);
         if (retryCount > 0) {
             const retryOperations = new LinearRetryPolicyFilter(retryCount, retryInterval);
             this.queueService = this.queueService.withFilter(retryOperations);
