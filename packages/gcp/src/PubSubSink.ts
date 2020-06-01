b92e5e6fa4833e87aed24b3ea2683709da3c331c
Kshitiz Gupta <kshitiz.gupta@walmart.com>
2020-06-01 18:59:21 -0400

    Add GCP PubSub Sink
    Add support for writing to Google Cloud Platform's PubSub.

---
+++ packages/gcp/src/PubSubSink.ts
@@ -0,0 +1,183 @@
+/*
+Copyright (c) Walmart Inc.
+
+This source code is licensed under the Apache 2.0 license found in the
+LICENSE file in the root directory of this source tree.
+*/
+
+import {
+    DefaultComponentContext,
+    IComponentContext,
+    IDisposable,
+    ILogger,
+    IOutputSink,
+    IOutputSinkGuarantees,
+    IPublishedMessage,
+    IRequireInitialization,
+    OutputSinkConsistencyLevel,
+    IMetrics,
+    OpenTracingTagKeys,
+    failSpan,
+} from "@walmartlabs/cookie-cutter-core";
+import { Span, SpanContext, Tags, Tracer } from "opentracing";
+import { PubSub, Attributes } from "@google-cloud/pubsub";
+import { IGcpAuthConfiguration, IPubSubPublisherConfiguration } from ".";
+
+interface IPayloadWithAttributes {
+    payload: Buffer;
+    attributes: Attributes;
+    spanContext: SpanContext;
+}
+
+enum PubSubMetricResults {
+    Success = "success",
+    Error = "error",
+}
+
+enum PubSubMetrics {
+    MsgPublished = "cookie_cutter.pubsub_sink.msg_published",
+}
+
+export enum PubSubMetadata {
+    Topic = "topic",
+}
+
+enum PubSubOpenTracingTagKeys {
+    TopicName = "pubSub.topic",
+}
+
+export const AttributeNames = {
+    eventType: "eventType",
+    timestamp: "dt",
+    contentType: "mimeType",
+};
+
+/*
+ * Output sink to produce to Google's PubSub topics.
+ */
+export class PubSubSink
+    implements IOutputSink<IPublishedMessage>, IRequireInitialization, IDisposable {
+    private producer: PubSub;
+    private logger: ILogger;
+    private tracer: Tracer;
+    private metrics: IMetrics;
+    private spanOperationName: string = "Write to Google PubSub";
+
+    constructor(private readonly config: IGcpAuthConfiguration & IPubSubPublisherConfiguration) {
+        this.logger = DefaultComponentContext.logger;
+        this.tracer = DefaultComponentContext.tracer;
+    }
+
+    public async initialize(ctx: IComponentContext): Promise<void> {
+        this.logger = ctx.logger;
+        this.tracer = ctx.tracer;
+        this.metrics = ctx.metrics;
+        this.producer = new PubSub({
+            projectId: this.config.projectId,
+            credentials: {
+                client_email: this.config.clientEmail,
+                private_key: this.config.privateKey,
+            },
+        });
+    }
+
+    private spanLogAndSetTags(span: Span, funcName: string, topic: string): void {
+        span.log({ topic });
+        span.setTag(Tags.SPAN_KIND, Tags.SPAN_KIND_RPC_CLIENT);
+        span.setTag(Tags.COMPONENT, "cookie-cutter-pubSub");
+        span.setTag(OpenTracingTagKeys.FunctionName, funcName);
+        span.setTag(PubSubOpenTracingTagKeys.TopicName, topic);
+    }
+
+    private emitMetrics(topic: string, eventType: string, result: PubSubMetricResults) {
+        this.metrics.increment(PubSubMetrics.MsgPublished, {
+            topic,
+            event_type: eventType,
+            result,
+        });
+    }
+
+    public async sink(output: IterableIterator<IPublishedMessage>): Promise<void> {
+        const messagesByTopic: Map<string, IPayloadWithAttributes[]> = new Map();
+        for (const msg of output) {
+            const topic = msg.metadata[PubSubMetadata.Topic] || this.config.defaultTopic;
+            if (!messagesByTopic.has(topic)) {
+                messagesByTopic.set(topic, []);
+            }
+
+            const formattedMsg = this.formatMessage(msg);
+            messagesByTopic.get(topic).push(formattedMsg);
+        }
+        for (const [topic, messages] of messagesByTopic) {
+            const batchPublisher = this.producer.topic(topic, {
+                batching: {
+                    maxBytes: this.config.maxPayloadSize,
+                    maxMessages: this.config.maximumBatchSize,
+                    maxMilliseconds: this.config.maximumBatchWaitTime,
+                },
+            });
+            for (const message of messages) {
+                const span = this.tracer.startSpan(this.spanOperationName, {
+                    childOf: message.spanContext,
+                });
+                this.spanLogAndSetTags(span, this.sink.name, topic);
+                try {
+                    const messageId = await batchPublisher.publish(
+                        message.payload,
+                        message.attributes
+                    );
+                    span.log({ messageId });
+                    this.emitMetrics(
+                        topic,
+                        message.attributes[AttributeNames.eventType],
+                        PubSubMetricResults.Success
+                    );
+                    this.logger.debug("Message published to PubSub", { topic, messageId });
+                } catch (e) {
+                    failSpan(span, e);
+                    messages.forEach((message) =>
+                        this.emitMetrics(
+                            topic,
+                            message.attributes[AttributeNames.eventType],
+                            PubSubMetricResults.Success
+                        )
+                    );
+                    throw e;
+                } finally {
+                    span.finish();
+                }
+            }
+        }
+    }
+
+    public async dispose(): Promise<void> {
+        if (this.producer) {
+            await this.producer.close();
+        }
+    }
+
+    public get guarantees(): IOutputSinkGuarantees {
+        return {
+            consistency: OutputSinkConsistencyLevel.None,
+            idempotent: false,
+            maxBatchSize: this.config.maximumBatchSize,
+        };
+    }
+
+    private formatMessage(msg: IPublishedMessage): IPayloadWithAttributes {
+        const timestamp = Date.now().toString();
+        const payload: Buffer | null = Buffer.from(this.config.encoder.encode(msg.message));
+
+        const attributes: Attributes = {
+            [AttributeNames.timestamp]: timestamp,
+            [AttributeNames.eventType]: msg.message.type,
+            [AttributeNames.contentType]: this.config.encoder.mimeType,
+        };
+
+        return {
+            payload,
+            attributes,
+            spanContext: msg.spanContext,
+        };
+    }
+}
--- packages/gcp/src/config.ts
+++ packages/gcp/src/config.ts
@@ -5,8 +5,13 @@ This source code is licensed under the Apache 2.0 license found in the
 LICENSE file in the root directory of this source tree.
 */
 
-import { config } from "@walmartlabs/cookie-cutter-core";
-import { IBigQueryConfiguration, IGCSConfiguration } from ".";
+import { config, IMessageEncoder } from "@walmartlabs/cookie-cutter-core";
+import {
+    IBigQueryConfiguration,
+    IGCSConfiguration,
+    IGcpAuthConfiguration,
+    IPubSubPublisherConfiguration,
+} from ".";
 
 @config.section
 export class GCSConfiguration implements IGCSConfiguration {
@@ -74,6 +79,73 @@ export class BigQueryConfiguration implements IBigQueryConfiguration {
         config.noop();
     }
     public get privateKey(): string {
+        return config.noop();
+    }
+}
+
+@config.section
+export class GcpAuthConfiguration implements IGcpAuthConfiguration {
+    @config.field(config.converters.string)
+    public set projectId(_: string) {
+        config.noop();
+    }
+    public get projectId(): string {
+        return config.noop();
+    }
+
+    @config.field(config.converters.string)
+    public set clientEmail(_: string) {
+        config.noop();
+    }
+    public get clientEmail(): string {
+        return config.noop();
+    }
+
+    @config.field(config.converters.string)
+    public set privateKey(_: string) {
+        config.noop();
+    }
+    public get privateKey(): string {
+        return config.noop();
+    }
+}
+
+@config.section
+export class PubSubPublisherConfiguration extends GcpAuthConfiguration
+    implements IPubSubPublisherConfiguration {
+    @config.field(config.converters.none)
+    public set encoder(_: IMessageEncoder) {
+        config.noop();
+    }
+    public get encoder(): IMessageEncoder {
+        return config.noop();
+    }
+    @config.field(config.converters.string)
+    public set defaultTopic(_: string) {
+        config.noop();
+    }
+    public get defaultTopic(): string {
+        return config.noop();
+    }
+    @config.field(config.converters.number)
+    public set maximumBatchSize(_: number) {
+        config.noop();
+    }
+    public get maximumBatchSize(): number {
+        return config.noop();
+    }
+    @config.field(config.converters.timespan)
+    public set maximumBatchWaitTime(_: number) {
+        config.noop();
+    }
+    public get maximumBatchWaitTime(): number {
+        return config.noop();
+    }
+    @config.field(config.converters.bytes)
+    public set maxPayloadSize(_: number) {
+        config.noop();
+    }
+    public get maxPayloadSize(): number {
         return config.noop();
     }
 }
