9be08fe45676298749cc696fc82338aeb04a7bee
Plamen Ivanov <plamen.ivanov@jet.com>
2020-06-18 17:39:48 -0400

    Basic producer and consumer scripts.

---
+++ packages/amqp/src/amq_producer.js
@@ -0,0 +1,28 @@
+var q = 'testQueue';
+
+var amqp = require('amqplib/callback_api');
+
+amqp.connect('amqp://localhost', function(error0, connection) {
+    if (error0) {
+        throw error0;
+    }
+    connection.createChannel(function(error1, channel) {
+        if (error1) {
+            throw error1;
+        }
+
+        var queue = q;
+        var msg = `Hello World! ${Date.now()}`;
+
+        channel.assertQueue(queue, {
+            durable: true
+        });
+        channel.sendToQueue(queue, Buffer.from(msg));
+
+        console.log(" [x] Sent %s", msg);
+    });
+    setTimeout(function() {
+        connection.close();
+        process.exit(0);
+    }, 500);
+});
