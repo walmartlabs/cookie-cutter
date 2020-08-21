6c5ca8431129ef5d9eacb6643c8b983b4fa2dd38
Plamen Ivanov <plamen.ivanov@jet.com>
2020-08-21 16:49:48 -0400

    Implement IEncodedMessageEmbedder for  ProtoMessageEncoder

--- packages/proto/src/ProtoMessageEncoder.ts
+++ packages/proto/src/ProtoMessageEncoder.ts
@@ -5,13 +5,31 @@ This source code is licensed under the Apache 2.0 license found in the
 LICENSE file in the root directory of this source tree.
 */
 
-import { IMessage, IMessageEncoder } from "@walmartlabs/cookie-cutter-core";
+import {
+    IMessage,
+    IMessageEncoder,
+    IEncodedMessageEmbedder,
+} from "@walmartlabs/cookie-cutter-core";
 import { IProtoMessageEncoderRegistry } from ".";
+import { isString } from "util";
 
-export class ProtoMessageEncoder implements IMessageEncoder {
+export class ProtoMessageEncoder implements IMessageEncoder, IEncodedMessageEmbedder {
     public readonly mimeType: string = "application/x-protobuf";
 
-    constructor(private readonly lookup: IProtoMessageEncoderRegistry) {}
+    constructor(
+        private readonly lookup: IProtoMessageEncoderRegistry,
+        private readonly base64Encode: boolean = false
+    ) {}
+
+    public toJsonEmbedding(encoded: Uint8Array): any {
+        return this.base64Encode ? Buffer.from(encoded).toString("base64") : encoded;
+    }
+
+    public fromJsonEmbedding(embedding: any): Uint8Array {
+        return isString(embedding)
+            ? new Buffer(embedding as string, "base64")
+            : (embedding as Uint8Array);
+    }
 
     public encode(msg: IMessage): Uint8Array {
         const encoder = this.lookup.toEncoder(msg.type);
