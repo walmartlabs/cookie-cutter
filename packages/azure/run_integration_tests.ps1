c44a4cdfe3b0752dd6c676c715d5be2bea9f6e19
Emma Lynch <emmalynch@users.noreply.github.com>
2020-09-01 11:57:47 +0100

    [full ci]

--- packages/azure/run_integration_tests.ps1
+++ packages/azure/run_integration_tests.ps1
@@ -1,18 +1,15 @@
-function printAndLog
-{
-    param([string]$argstring = "unknown")
-    Write-Host $argstring
-    Add-Content C:\vagrant\service_loop.txt $argstring
-}
+ECHO "Installing Curl"
 
 choco install curl -y
+
+ECHO "Starting Downloads"
 
 curl -o az_storage_emulator.msi 'https://go.microsoft.com/fwlink/?linkid=717179&clcid=0x409' 
 curl -o cosmos.msi 'https://aka.ms/cosmosdb-emulator'
 curl -o nssm.zip 'https://nssm.cc/release/nssm-2.24.zip'
 curl -o SQLLocalDB.MSI 'https://go.microsoft.com/fwlink/?LinkID=866658'
 
-printAndLog -argstring "Downloads Completed"
+ECHO "Downloads Completed"
 
 # Cosmos DB emulator
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
@@ -31,12 +28,12 @@ C:\nssm\nssm-2.24\win64\nssm.exe start cosmosdbemulator
 Set-ItemProperty -Name 'FailureActions' -Path 'HKLM:\HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\cosmosdbemulator' -Value ([byte[]](0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x3,0x0,0x0,0x0,0x14,0x0,0x0,0x0,0x1,0x0,0x0,0x0,0x60,0xea,0x0,0x0,0x1,0x0,0x0,0x0,0x60,0xea,0x0,0x0,0x1,0x0,0x0,0x0,0x60,0xea,0x0,0x0))
 Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
 
-printAndLog -argstring "Started Cosmos Process"
+ECHO  "Started Cosmos Process"
 
 # SQL DB
 Start-Process -wait msiexec -ArgumentList "/i","C:\SqlLocalDB.MSI","/qn","IACCEPTSQLLOCALDBLICENSETERMS=YES"
 
-printAndLog -argstring "Started SQL DB"
+ECHO  "Started SQL DB"
 
 # Azure storage emulator
 Start-Process -wait C:\az_storage_emulator.msi -ArgumentList "/quiet"
@@ -45,16 +42,16 @@ $vm_ip = (Get-NetIPAddress -InterfaceAlias "Ethernet" -AddressFamily "IPv4").IPA
 $storage_emulator_config_path = "C:\Program Files (x86)\Microsoft SDKs\Azure\Storage Emulator\AzureStorageEmulator.exe.config"
 ((Get-Content -path $storage_emulator_config_path -Raw) -replace '127.0.0.1', $vm_ip ) | Set-Content -Path $storage_emulator_config_path
 
-printAndLog -argstring "Started Azure Storage Emulator"
+ECHO "Started Azure Storage Emulator"
 
 
-printAndLog -argstring "Exporting env variables"
+ECHO "Exporting env variables"
 
 set NODE_TLS_REJECT_UNAUTHORIZED="0"
 set COSMOS_SECRET_KEY=‘"2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw=="
 set AZURE_STORAGE_CONNECTION_STRING="DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;TableEndpoint=http://127.0.0.1:10002/devstoreaccount1;QueueEndpoint=http://127.0.0.1:10001/devstoreaccount1;"
 set AZURE_STORAGE_ACCESS_KEY="Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw=="
 
-printAndLog -argstring "Starting tests"
+ECHO "Starting tests"
 
 jest --config=../../jest.integration.config.js --rootDir=.
