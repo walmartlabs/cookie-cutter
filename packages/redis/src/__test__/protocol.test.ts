2b10f5b4e17777d605fac69eaf31dce4aef8cfa2
sklose <sebastian.klose@walmart.com>
2020-12-14 14:25:24 -0500

    fix redis client skipping over messages when there is an invalid message in a batch

--- packages/redis/src/__test__/protocol.test.ts
+++ packages/redis/src/__test__/protocol.test.ts
@@ -2,7 +2,7 @@ import { parseRawReadGroupResult } from "../RedisClient";
 import { RawReadGroupResult } from "../RedisProxy";
 
 describe("XReadGroup response parsing", () => {
-    it("parses single stream result", () => {
+    it("parses multi stream result", () => {
         const data: RawReadGroupResult = [
             [
                 "stream-1",
@@ -24,5 +24,27 @@ describe("XReadGroup response parsing", () => {
 
         const messages = parseRawReadGroupResult(data);
         expect(messages).toHaveLength(6);
+    });
+
+    it("parses result with bad messages", () => {
+        const data: RawReadGroupResult = [
+            [
+                "stream-1",
+                [
+                    ["1597844517517-0", ["redis.stream.key", '{"foo": "bar"}']],
+                    ["1597844517519-0", []],
+                    ["1597844517952-0", ["redis.stream.key", '{"fizz": "buzz"}']],
+                    ["1597844518432-0", ["redis.stream.key", '{"bar": "foo"}']],
+                ],
+            ],
+        ] as any;
+
+        const messages = parseRawReadGroupResult(data);
+        expect(messages).toMatchObject([
+            { streamName: "stream-1", messageId: "1597844517517-0", data: '{"foo": "bar"}' },
+            { streamName: "stream-1", messageId: "1597844517519-0" },
+            { streamName: "stream-1", messageId: "1597844517952-0", data: '{"fizz": "buzz"}' },
+            { streamName: "stream-1", messageId: "1597844518432-0", data: '{"bar": "foo"}' },
+        ]);
     });
 });
