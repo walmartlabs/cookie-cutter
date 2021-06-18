c565b5a0abc2c8c12b0f808fd8bed7c3138df2b4
Plamen Ivanov <plamen.ivanov@jet.computer>
2021-06-18 16:07:36 -0400

    Try [full ci]

--- packages/azure/run_integration_tests.ps1
+++ packages/azure/run_integration_tests.ps1
@@ -1,11 +1,11 @@
 # $ErrorActionPreference = "Stop"
 
-Write-Host "Throw Error here"
-If ($lastExitCode -eq "0") {
-    Write-Host "Success !!"
-}
-Write-Host "Last Exit Code: $lastExitCode"
-throw "This is an error."
+# Write-Host "Throw Error here"
+# If ($lastExitCode -ne "0") {
+#     Write-Host "Success !!"
+# }
+# Write-Host "Last Exit Code: $lastExitCode"
+# throw "This is an error."
 
 Write-Host "Build core"
 cd packages/core
@@ -15,7 +15,12 @@ Write-Host "Integrate azure"
 cd ../azure
 yarn build
 yarn integrate
-# exit_code=$?
+$exit_code = $?
+Write-Host "EXIT CODE: $exit_code"
+# If ("$?" -ne "0") {
+If ("$exit_code" -ne "0") {
+    throw "Yarn Integrate Failed"
+}
 
 Start-Sleep -s 60
 # exit $exit_code
