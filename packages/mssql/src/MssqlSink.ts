39af08a961fada30c30b9a6ee24bbe6dda4e3bf8
Plamen Ivanov <plamen.ivanov@jet.computer>
2021-07-28 13:00:44 -0400

    trigger regular build

--- packages/mssql/src/MssqlSink.ts
+++ packages/mssql/src/MssqlSink.ts
@@ -481,7 +481,6 @@ export class MssqlSink
         request: sql.Request
     ): Promise<ITableDetails> {
         let tableDetails: ITableDetails = this.tableMap.get(`${schema}.${tableName}`);
-
         if (!tableDetails) {
             tableDetails = await this.getTableDetailsFromSql(schema, tableName, request);
             this.tableMap.set(`${schema}.${tableName}`, tableDetails);
