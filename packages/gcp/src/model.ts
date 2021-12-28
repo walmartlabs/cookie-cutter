c64e53368bd77cd167cce91d3a5a5937a7eadc0a
Manasa Tully <manasatully@gmail.com>
2021-12-28 11:09:05 -0500

    address linter issues

--- packages/gcp/src/model.ts
+++ packages/gcp/src/model.ts
@@ -1,17 +1,17 @@
 export interface IRawPubSubMessage {
     // id of message
-    id: string,
+    id: string;
     // id used to ack message receival
-    ackId: string,
+    ackId: string;
     // the data field. If empty must contain at least one attribute
-    data?: string,
+    data?: string;
     // attributes for message. This can be used to filter messages on subscription
-    attributes?: Map<string, string>,
+    attributes?: Map<string, string>;
     /*
-    * Date when pub/sub server received the message
-    * Time stampin RFC3339 UTC Zulu format with nanosecond resolution upto nine fractional digits
-    * https://cloud.google.com/pubsub/docs/reference/rest/v1/PubsubMessage
-    */
-    publishTime: string,
-    orderingKey?: string,
+     * Date when pub/sub server received the message
+     * Time stampin RFC3339 UTC Zulu format with nanosecond resolution upto nine fractional digits
+     * https://cloud.google.com/pubsub/docs/reference/rest/v1/PubsubMessage
+     */
+    publishTime: string;
+    orderingKey?: string;
 }
