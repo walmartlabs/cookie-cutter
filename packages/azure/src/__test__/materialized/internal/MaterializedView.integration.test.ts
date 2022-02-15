a61e1ca4ea2854a4492d002f1b8c5e0cf107347f
Plamen Ivanov <plamen.ivanov@jet.computer>
2022-02-15 17:20:37 +0200

    [full ci]

--- packages/azure/src/__test__/materialized/internal/MaterializedView.integration.test.ts
+++ packages/azure/src/__test__/materialized/internal/MaterializedView.integration.test.ts
@@ -19,7 +19,6 @@ import { DummyState } from "../../dummystate";
 import { setup, teardown } from "../../integrationSetup";
 
 jest.setTimeout(10000);
-
 const url = "https://localhost:8081";
 const key = process.env.COSMOS_SECRET_KEY;
 const databaseId = "materialized-view-integration-test";
