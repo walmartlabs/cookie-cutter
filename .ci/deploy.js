12072a197fdf103617113fb262a5e4003b119700
Plamen Ivanov <plamen.ivanov@jet.computer>
2022-07-12 18:08:08 -0400

    Test if error still happens

--- .ci/deploy.js
+++ .ci/deploy.js
@@ -51,7 +51,8 @@ function deploy(packagePath) {
         console.log(`publishing ${name}@${version} to ${tag}`);
         copyFileSync(join(__dirname, "..", ".yarnignore"), join(fullPath, ".yarnignore"))
         try {
-            yarn(`publish --cwd="${fullPath}" --tag=${tag} --access=public --non-interactive`, false);
+            // NOTE: disabled actual publish step while testing
+            // yarn(`publish --cwd="${fullPath}" --tag=${tag} --access=public --non-interactive`, false);
         } finally {
             unlinkSync(join(fullPath, ".yarnignore"));
         }
